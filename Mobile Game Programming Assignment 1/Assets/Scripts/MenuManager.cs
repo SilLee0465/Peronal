73dcdfe144adedb975acee96eb5cf4831760140a
SilLee <61728125+SilLee0465@users.noreply.github.com>
2020-11-22 23:48:04 +0800

    Updated UI stuff

--- Mobile Game Programming Assignment 1/Assets/Scripts/MenuManager.cs
+++ Mobile Game Programming Assignment 1/Assets/Scripts/MenuManager.cs
@@ -8,16 +8,19 @@ public class MenuManager : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene("Runner");
+        Resume();
     }
 
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Resume();
     }
 
     public void MainMenu()
     {
         SceneManager.LoadScene("Main Menu");
+        Resume();
     }
 
     public void QuitGame()
